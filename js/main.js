bb99900ceeac829c890d710550a640899fbda0c8
hjs <85000175@qq.com>
2017-03-07 16:30:35 +0800

    modify: inject debug scripts when document.onreadystatechange ===  'interactive'; debug script prepend to document.head;

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 "use strict";
-// document.addEventListener('DOMContentLoaded', function(){
-    
-// }, false);
+document.onreadystatechange = function(){
+	if(document.readyState == 'interactive'){
+		chrome.runtime.sendMessage('queryRunningState', function(response) {
+		    if(response === 'on'){
+				var n=document.createElement("script");
+		        //将本content_script中的debug函数转换成字符串，再注入到web页面中
+		        n.text = 'window.debug='+ window.debug.toString();
+		        document.head.insertBefore(n, document.head.firstChild);
+
+		        console.log('debug script injected!');
+		        // debugger;
+		    }
+		});
+	}
+};
 
 /*
 content_scripts所读取的localStorage时web页面的
@@ -12,13 +24,14 @@ content_script的window对象上添加一个debug函数。
 该window对象独立于web页面上的window，或插件background page上的window，
 它们存在于各自的沙箱中。
 */
-chrome.runtime.sendMessage('queryRunningState', function(response) {
-    if(response === 'on'){
-        var n=document.createElement("script");
-        //将本content_script中的debug函数转换成字符串，再注入到web页面中
-        n.text = 'window.debug='+ window.debug.toString();
-        document.head.appendChild(n);
+// chrome.runtime.sendMessage('queryRunningState', function(response) {
+//     if(response === 'on'){
+//         var n=document.createElement("script");
+//         //将本content_script中的debug函数转换成字符串，再注入到web页面中
+//         n.text = 'window.debug='+ window.debug.toString();
+//         document.head.insertBefore(n, document.head.firstChild);
 
-        console.log('debug script injected!');
-    }
-});
+//         console.log('debug script injected!');
+//         // debugger;
+//     }
+// });
